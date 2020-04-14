eee2f5528660204396543bf89355c03b7edd1d5e
De Xing <dzx0010@auburn.edu>
2020-04-13 20:53:56 -0500

    change output name format

--- Lindel_prediction.py
+++ Lindel_prediction.py
@@ -18,7 +18,8 @@ seq = sys.argv[1].upper() #input your sequence here
 filename = sys.argv[2]
 try:
     y_hat, fs = gen_prediction(seq,weights,prerequesites)
-    filename += '_fs=' + str(round(fs,3))+'.txt'
+    filename += '_fs_' + str(round(fs,3))+'.txt'
+'''=is special character which is not good use in filename'''
     rev_index = prerequesites[1]
     pred_freq = {}
     for i in range(len(y_hat)):
